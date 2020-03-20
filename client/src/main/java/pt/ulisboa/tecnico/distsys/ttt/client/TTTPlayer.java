7ae73dcb95c7ebd4c79bffba6b7b642d13a76f94
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2020-03-20 23:11:10 +0000

    Add debug flag and messages Use try with scanner resource

--- client/src/main/java/pt/ulisboa/tecnico/distsys/ttt/client/TTTPlayer.java
+++ client/src/main/java/pt/ulisboa/tecnico/distsys/ttt/client/TTTPlayer.java
@@ -13,125 +13,162 @@ import pt.ulisboa.tecnico.distsys.ttt.contract.PlayResult;
 
 public class TTTPlayer {
 
-    public static void main(String[] args) {
-        System.out.println(TTTPlayer.class.getSimpleName());
+	/** Set flag to true to print debug messages. The flag can be set using the -Ddebug command line option. */
+	private static final boolean DEBUG_FLAG = (System.getProperty("debug") != null);
 
-        // receive and print arguments
-        System.out.printf("Received %d arguments%n", args.length);
-        for (int i = 0; i < args.length; i++) {
-            System.out.printf("arg[%d] = %s%n", i, args[i]);
-        }
+	/** Helper method to print debug messages. */
+	private static void debug(String debugMessage) {
+		if (DEBUG_FLAG)
+			System.err.println(debugMessage);
+	}
 
-        // check arguments
-        if (args.length < 2) {
-            System.err.println("Argument(s) missing!");
-            System.err.printf("Usage: java %s host port%n", TTTPlayer.class.getName());
-            return;
-        }
+	/**
+	 * During the exercise, there are two code alternatives for calling play. Set
+	 * flag to true to play using a POST with a body. Set flag to false to use a GET
+	 * with arguments.
+	 */
+	private static final boolean USE_POST_TO_PLAY_FLAG = true;
 
-        final String host = args[0];
-        final int port = Integer.parseInt(args[1]);
-        final String target = host + ":" + port;
-        
-        String restURL = "http://" + target + "/ttt/game";
-        
-        playGame(restURL);
+	/** Main method. */
+	public static void main(String[] args) {
+		System.out.println(TTTPlayer.class.getSimpleName());
 
-    }
+		// receive and print arguments
+		System.out.printf("Received %d arguments%n", args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.printf("arg[%d] = %s%n", i, args[i]);
+		}
 
-    private static void playGame(String restURL) {
-    	
-    	Client client = ClientBuilder.newClient();
-    	
-    	/* Reset board to play a new game */
-    	//client.target(restURL).path("board").path("reset").request().get(String.class);
+		// check arguments
+		if (args.length < 2) {
+			System.out.println("Argument(s) missing!");
+			System.out.printf("Usage: java %s host port%n", TTTPlayer.class.getName());
+			return;
+		}
+		final String host = args[0];
+		final int port = Integer.parseInt(args[1]);
+		final String target = host + ":" + port;
 
-        int player = 0;                              /* Player number - 0 or 1               */
-        int go = 0;                                  /* Square selection number for turn     */
-        int row = 0;                                 /* Row index for a square               */
-        int column = 0;                              /* Column index for a square            */
-        int winner = -1;                             /* The winning player                   */
-        PlayResult play_res;
+		String restURL = "http://" + target + "/ttt/game";
+		debug("URL: " + restURL);
 
-        /* The main game loop. The game continues for up to 9 turns */
-        /* As long as there is no winner                            */
-        do {
-            /* Get valid player square selection */
-            do {
-                /* Print current board */
-                System.out.println(client.target(restURL).path("board").request().get(String.class));
+		playGame(restURL);
+	}
 
-                System.out.printf("\nPlayer %d, please enter the number of the square " +
-                        "where you want to place your %c (or 0 to refresh the board): ", player, (player==1)?'X':'O');
-                go = readPlay();
+	private static void playGame(String restURL) {
 
-                if (go == 0){
-                    play_res = PlayResult.UNKNOWN;
-                    continue;
-                }
+		Client client = ClientBuilder.newClient();
 
-                row = --go/3;                                 /* Get row index of square      */
-                column = go%3;                                /* Get column index of square   */
+		int player = 0; /* Player number - 0 or 1 */
+		int go = 0; /* Square selection number for turn */
+		int row = 0; /* Row index for a square */
+		int column = 0; /* Column index for a square */
+		int winner = -1; /* The winning player */
+		PlayResult play_res;
 
-                PlayRequest playRequest = new PlayRequest(row, column, player);
-                
-                /* Uncomment to use URL parameter to play */
-                /* URL to play is: play/{row}/{column}/{player} */
-                /* String playString = "play/" +  String.valueOf(row) + '/' + String.valueOf(column) + '/' + String.valueOf(player); */
-                                
-                Response response = client.target(restURL).path("play").request(MediaType.APPLICATION_JSON)
-              	      .post(Entity.entity(playRequest, MediaType.APPLICATION_JSON), Response.class);
-                
-                /* Use for debug */
-                /* System.out.println(response.getStatus()); */
-                
-                play_res = response.readEntity(PlayResult.class);
-                /* play_res = client.target(restURL).path(playString).request().get(PlayResult.class); */
-                
-                if (play_res != PlayResult.SUCCESS) {
-                    displayResult(play_res);
-                }
-            } while(play_res != PlayResult.SUCCESS);
+		/*
+		 * Using try with scanner - ensures the resource is closed in the end, even if
+		 * there are exceptions.
+		 */
+		try (Scanner scanner = new Scanner(System.in)) {
 
-            winner = client.target(restURL).path("board/checkwinner").request().get(Integer.class);
-            player = (player+1)%2;                           /* Select player */
+			/* The main game loop. The game continues for up to 9 turns, */
+			/* as long as there is no winner. */
+			do {
+				/* Get valid player square selection. */
+				do {
+					/* Print current board. */
+					debug("Calling GET board");
+					String board = client.target(restURL).path("board").request().get(String.class);
+					System.out.println(board);
 
-            System.out.println("player " + player);
+					System.out.printf(
+							"\nPlayer %d, please enter the number of the square "
+									+ "where you want to place your %c (or 0 to refresh the board): ",
+							player, (player == 1) ? 'X' : 'O');
+					go = scanner.nextInt();
 
-        } while (winner == -1);
+					debug("go = " + go);
 
-        /* Game is over so display the final board */
-        System.out.println(client.target(restURL).path("board").request().get(String.class));
+					if (go == 0) {
+						play_res = PlayResult.UNKNOWN;
+						continue;
+					}
 
-        /* Display result message */
-        if(winner == 2)
-            System.out.println("\nHow boring, it is a draw");
-        else
-            System.out.println("\nCongratulations, player " + winner + ", YOU ARE THE WINNER!");
-    }
+					row = --go / 3; /* Get row index of square. */
+					column = go % 3; /* Get column index of square. */
+					debug("row = " + row + ", column = " + column);
 
-    private static void displayResult(PlayResult play_res) {
-        switch (play_res) {
-            case OUT_OF_BOUNDS:
-                System.out.print("Position outside board.");
-                break;
-            case SQUARE_TAKEN:
-                System.out.print("Square already taken.");
-                break;
-            case WRONG_TURN:
-                System.out.print("Not your turn.");
-                break;
-            case GAME_FINISHED:
-                System.out.print("Game has finished.");
-                break;
-            default:
-            	break;
-        }
-        System.out.println(" Try again...");
-    }
+					PlayRequest playRequest = new PlayRequest(row, column, player);
 
-    private static int readPlay() {
-		Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
-    }
+					Response response = null;
+					if (USE_POST_TO_PLAY_FLAG) {
+						/* Use POST to play. */
+						debug("Calling POST play");
+						response = client.target(restURL).path("play").request(MediaType.APPLICATION_JSON)
+								.post(Entity.entity(playRequest, MediaType.APPLICATION_JSON), Response.class);
+						debug("Response status: " + response.getStatus() + " " + response.getStatusInfo());
+						play_res = response.readEntity(PlayResult.class);
+					} else {
+						/* Use URL parameter to play. */
+						/* URL to play is: play/{row}/{column}/{player} */
+						String playPath = "play/" + String.valueOf(row) + '/' + String.valueOf(column) + '/'
+								+ String.valueOf(player);
+						debug("Calling GET " + playPath);
+						play_res = client.target(restURL).path(playPath).request().get(PlayResult.class);
+					}
+
+					if (play_res != PlayResult.SUCCESS) {
+						displayResult(play_res);
+					}
+
+				} while (play_res != PlayResult.SUCCESS);
+
+				debug("Calling GET checkwinner");
+				winner = client.target(restURL).path("board/checkwinner").request().get(Integer.class);
+
+				/* Select next player. */
+				player = (player + 1) % 2;
+
+				System.out.println("player " + player);
+
+			} while (winner == -1);
+
+			/* Game is over so display the final board. */
+			debug("Calling GET board");
+			System.out.println(client.target(restURL).path("board").request().get(String.class));
+
+			/* Display result message. */
+			if (winner == 2) {
+				System.out.println();
+				System.out.println("How boring, it is a draw");
+			} else {
+				System.out.println();
+				System.out.println("Congratulations, player " + winner + ", YOU ARE THE WINNER!");
+			}
+
+		}
+	}
+
+	/** Helper method to display result as text. */
+	private static void displayResult(PlayResult play_res) {
+		switch (play_res) {
+		case OUT_OF_BOUNDS:
+			System.out.print("Position outside board.");
+			break;
+		case SQUARE_TAKEN:
+			System.out.print("Square already taken.");
+			break;
+		case WRONG_TURN:
+			System.out.print("Not your turn.");
+			break;
+		case GAME_FINISHED:
+			System.out.print("Game has finished.");
+			break;
+		default:
+			break;
+		}
+		System.out.println(" Try again...");
+	}
+
 }
