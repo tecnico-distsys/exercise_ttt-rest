98b05e02b9a170d93c98395a5bf7197ecf98773a
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2021-03-07 10:35:12 +0000

    Rename, move package

--- client/src/main/java/pt/tecnico/ttt/client/TTTPlayer.java
+++ client/src/main/java/pt/tecnico/ttt/client/TTTPlayer.java
@@ -1,15 +1,15 @@
-package pt.ulisboa.tecnico.distsys.ttt.client;
+package pt.tecnico.ttt.client;
 
 import java.util.Scanner;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-import pt.ulisboa.tecnico.distsys.ttt.contract.PlayRequest;
-import pt.ulisboa.tecnico.distsys.ttt.contract.PlayResult;
+import pt.tecnico.ttt.contract.PlayRequest;
+import pt.tecnico.ttt.contract.PlayResult;
 
 public class TTTPlayer {
 
