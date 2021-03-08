2188fbaa706d66423883017869b912cfe9b052fb
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2021-03-08 19:56:37 +0000

    Receive configuration from arguments; add debug method

--- server/src/main/java/pt/tecnico/ttt/server/Main.java
+++ server/src/main/java/pt/tecnico/ttt/server/Main.java
@@ -12,21 +12,14 @@ import java.net.URI;
  *
  */
 public class Main {
-	// Base URI the Grizzly HTTP server will listen on
-	public static final String BASE_URI = "http://localhost:8080/ttt/";
+	
+	/** Set flag to true to print debug messages. The flag can be set using the -Ddebug command line option. */
+	private static final boolean DEBUG_FLAG = (System.getProperty("debug") != null);
 
-	/**
-	 * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-	 * @return Grizzly HTTP server.
-	 */
-	public static HttpServer startServer() {
-		// create a resource config that scans for JAX-RS resources and providers
-		// in example package
-		final ResourceConfig rc = new ResourceConfig().packages("pt.tecnico.ttt.server");
-
-		// create and start a new instance of grizzly http server
-		// exposing the Jersey application at BASE_URI
-		return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+	/** Helper method to print debug messages. */
+	private static void debug(String debugMessage) {
+		if (DEBUG_FLAG)
+			System.err.println(debugMessage);
 	}
 
 	/**
@@ -35,11 +28,57 @@ public class Main {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		final HttpServer server = startServer();
-		System.out.println(String.format("Jersey app started with WADL available at "
-				+ "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+		System.out.println(Main.class.getSimpleName());
+
+		// receive and print arguments
+		System.out.printf("Received %d arguments%n", args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.printf("arg[%d] = %s%n", i, args[i]);
+		}
+
+		// check arguments
+		if (args.length < 2) {
+			System.out.println("Argument(s) missing!");
+			System.out.printf("Usage: java %s host port%n", Main.class.getName());
+			return;
+		}
+		final String host = args[0];
+		final int port = Integer.parseInt(args[1]);
+		final String target = host + ":" + port;
+
+		/* Resources package name */
+		final String resourcesPackage = "pt.tecnico.ttt.server";
+		debug("Resources package: " + resourcesPackage);
+
+		/* Base URI the Grizzly HTTP server will listen on. */ 
+		final String baseURI = "http://" + target + "/ttt/";
+		debug("Base URL: " + baseURI);
+
+		/* URI for the Web Application Description Language document. */
+		final String wadlURI = baseURI + "application.wadl";
+		debug("WADL URL: " + wadlURI);
+
+		final HttpServer server = startServer(resourcesPackage, baseURI);
+		System.out.printf("Jersey app started with WADL available at %s%n", wadlURI);
+		
+		System.out.println("Hit enter to stop server...");
 		System.in.read();
+
 		server.shutdownNow();
 	}
-}
 
+	/**
+	 * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+	 * @return Grizzly HTTP server.
+	 */
+	public static HttpServer startServer(final String packageName, final String baseURI) {
+		// create a resource config that scans for JAX-RS resources and providers
+		// in example package
+		final ResourceConfig rc = new ResourceConfig().packages(packageName);
+
+		// create and start a new instance of grizzly http server
+		// exposing the Jersey application at BASE_URI
+		return GrizzlyHttpServerFactory.createHttpServer(URI.create(baseURI), rc);
+	}
+
+}
